22f41f03fee3c8a953390541982d46ea20bd71d5
mickiy123 <mikikinfe345@gmail.com>
2022-06-23 14:28:11 +0300

    Use different approach

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,25 +3,15 @@
 """ A module for lockboxes task"""
 
 
-def join(T, R):
-    """ A function that appends to a list"""
-    res = []
-    for e in R:
-        res += T[e]
-    return res
-
-
 def canUnlockAll(boxes):
     """ method that determines if all the boxes can be opened """
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-
-    return len(total) == len(boxes)
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
