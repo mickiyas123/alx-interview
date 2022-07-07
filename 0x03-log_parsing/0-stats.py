c32483c66808c1b907ad2008af9029b106a49392
mickiy123 <mikikinfe345@gmail.com>
2022-07-07 15:48:09 +0300

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,13 +4,12 @@ import sys
 
 status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 counts = {}
-all_stat_count = 0
 total_file_size = 0
 
 try:
     for line in sys.stdin:
         linesplit = line.split()
-        if (len(linesplit) > 6):
+        if len(linesplit) > 6:
             total_file_size += eval(linesplit[-1])
             status_code = eval(linesplit[-2])
             if (status_code in status_codes):
@@ -19,17 +18,18 @@ try:
                 else:
                     counts[status_code] = 1
 
-            if (sum(counts.values()) % 10 == 0):
-                print("File size: {}".format(total_file_size))
-                for key, val in sorted(counts.items()):
-                    print("{}: {}".format(key, val))
+        if (sum(counts.values()) % 10 == 0):
+            print("File size: {}".format(total_file_size))
+            for key, val in sorted(counts.items()):
+                print("{}: {}".format(key, val))
 
 except KeyboardInterrupt:
-    print("File size: {}".format(total_file_size))
+    print('File size: {}'.format(total_file_size))
     for key, val in sorted(counts.items()):
-        print("{}: {}".format(key, val))
+        print('{}: {}'.format(key, val))
     raise
+
 else:
-    print("File size: {}".format(total_file_size))
+    print('File size: {}'.format(total_file_size))
     for key, val in sorted(counts.items()):
-        print("{}: {}".format(key, val))
+        print('{}: {}'.format(key, val))
