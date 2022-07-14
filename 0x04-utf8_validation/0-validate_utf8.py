9e6ed97a8e3b1e7850e9d9225a847263ac75cb8f
mickiy123 <mikikinfe345@gmail.com>
2022-07-14 19:24:29 +0300

    Complete task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
-"""  A module that checks if a data is utf8 valid"""
+"""
+UTF-8 Validation
+"""
 
 
 def validUTF8(data):
     """
-    :type data: List[int]
-    :rtype: bool
+    data is a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
-    for num in data:
-        if num >= 256:
-            return False
-    return True
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
