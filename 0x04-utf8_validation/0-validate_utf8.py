715743f9b4896376bcf24bf205fe927b6f5e1163
mickiy123 <mikikinfe345@gmail.com>
2022-07-14 22:58:09 +0300

    Complete task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+"""
+Validate UTF-8
+
+"""
 
 
 def validUTF8(data):
     """
-    data is a list of integers
-    Return: True if data is a valid UTF-8
-    encoding, else return False
+    data: input list
+    Return: if data is a valid UTF-8 True
+            else False
     """
-    byte_count = 0
+    n_bytes = 0
 
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
-            byte_count -= 1
-    return byte_count == 0
+        n_bytes -= 1
 
-
+    return n_bytes == 0
